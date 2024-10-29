27a3d359bcc3c0b7ab9afc327dab2c39e7052814
Prince <prince_x4@live.in>
2024-10-29 03:01:00 +0000

    conditional_join

--- conditional_join.sql
+++ conditional_join.sql
@@ -2,11 +2,13 @@ with tb1(fid,fvalue) as (
 	values
 	(10,1),
 	(20,2),
-	(30,3)), tb2(fname, fvalue) as (
+	(30,3)
+), tb2(fname, fvalue) as (
 	values
 	('ten',1),
 	('twenty',2),
-	('thirty',3)), lkup_tb as (
+	('thirty',3)
+), lkup_tb as (
 	select fid, fname from tb1
 	join tb2 on (
 		case
@@ -14,7 +16,7 @@ with tb1(fid,fvalue) as (
 		when fid=20 then fname='twenty'
 		when fid=30 then fname='thirty'
 		else 0
-		end)
+		end )
 )
 
 select * from lkup_tb;
